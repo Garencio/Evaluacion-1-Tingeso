37b1efeacb47747d76afb5cc6c7b7c74b06617b4
Benjamin Pavez <102911491+Garencio@users.noreply.github.com>
2023-09-25 20:33:19 -0300

    Commit inicial

---
+++ src/main/java/com/evaluacion_1_tingeso/Evaluacion1TingesoApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion_1_tingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion1TingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacion1TingesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacion_1_tingeso/Evaluacion1TingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion_1_tingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion1TingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
